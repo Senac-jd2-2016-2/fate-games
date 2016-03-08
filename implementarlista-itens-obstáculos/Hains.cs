087dd045d9154fb2a25b19e73ea21ae289319f5e
MeruYabu <meru_yabu@hotmail.com>
2016-03-08 09:46:11 -0300

    Inicio

---
+++ implementarlista-itens-obstáculos/Hains.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace implementarlista_itens_obstáculos
+{
+    class Hains
+    {
+        public Rectangle HainsR; 
+        public int vida = 10, velocidade = 2;
+
+       
+       
+
+    }
+}
---
+++ implementarlista-itens-obstáculos/barra de itens.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace implementarlista_itens_obstáculos
+{
+    class barra_de_itens
+    {
+
+        public string[,] itens;
+
+        
+
+        
+
+       
+
+        
+    }
+}
